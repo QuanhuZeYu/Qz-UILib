d0b91ad4525b49a588431dac6e8e4456fcb71542
泉户泽雨 <1624910218@qq.com>
2025-05-07 23:12:24 +0800

    清理代码

--- src/main/java/club/heiqi/qz_uilib/ClientProxy.java
+++ src/main/java/club/heiqi/qz_uilib/ClientProxy.java
@@ -12,12 +12,12 @@ import org.apache.logging.log4j.Logger;
 
 public class ClientProxy extends CommonProxy {
     public static Logger LOG = LogManager.getLogger();
-    public GameMenu gameMenu;
+    /*public GameMenu gameMenu;*/
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-        gameMenu = new GameMenu().register();
+        /*gameMenu = new GameMenu().register();*/
         new RenderListener().register();
         new OpenTestGUI().register();
         new GaussianBlur();
--- src/main/java/club/heiqi/qz_uilib/CommonProxy.java
+++ src/main/java/club/heiqi/qz_uilib/CommonProxy.java
@@ -10,8 +10,6 @@ public class CommonProxy {
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-
-        MyMod.LOG.info("I am MyMod at version " + Tags.VERSION);
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
